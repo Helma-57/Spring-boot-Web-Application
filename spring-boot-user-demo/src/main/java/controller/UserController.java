24bc0e940cdf50d15541cbdb3acb987a7fbc948e
Helma <makhubelehelma@gmail.com>
2025-05-14 09:37:00 +0200

    adding rest controllers and mapping

--- spring-boot-user-demo/src/main/java/controller/UserController.java
+++ spring-boot-user-demo/src/main/java/controller/UserController.java
@@ -3,3 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
