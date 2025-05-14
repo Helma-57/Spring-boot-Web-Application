ec1d30e464672047d563995d18301f5ad819e873
Helma <makhubelehelma@gmail.com>
2025-05-14 09:35:54 +0200

    adding controller package

---
+++ spring-boot-user-demo/src/main/java/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
