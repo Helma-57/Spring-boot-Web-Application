1322f6b8490a189b311d722378e8d9af61579d08
Helma <makhubelehelma@gmail.com>
2025-05-14 09:16:00 +0200

    adding a public class for implementation of the interface

--- spring-boot-user-demo/src/main/java/com/example/demo/repo/FakeRepo.java
+++ spring-boot-user-demo/src/main/java/com/example/demo/repo/FakeRepo.java
@@ -5,3 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+
+@Repository
+public class FakeRepo implements FakeRepoInterface {
+    private List<User> users = new ArrayList<>();
