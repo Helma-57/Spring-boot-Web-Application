44769d6d7b8e652cd32684e13121c58e50012adc
Helma <makhubelehelma@gmail.com>
2025-05-14 09:43:24 +0200

    add constructor-based dependency injection for UserService

--- spring-boot-user-demo/src/main/java/controller/UserController.java
+++ spring-boot-user-demo/src/main/java/controller/UserController.java
@@ -9,3 +9,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
